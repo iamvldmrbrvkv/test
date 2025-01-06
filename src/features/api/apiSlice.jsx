ba7acc494fa1a6ed8008213349462b81857d258e
Владимир Боровиков <iamvladimirborovikov@gmail.com>
2025-01-07 00:27:28 +0300

    fix

--- src/features/api/apiSlice.jsx
+++ src/features/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/src/mocks' }),
+  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
   endpoints: (builder) => ({
     getTest: builder.query({
       query: () => '/data',
