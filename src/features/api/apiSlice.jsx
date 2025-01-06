90bbb130629c5a6b2245ffae7ce91298633e6beb
Владимир Боровиков <iamvladimirborovikov@gmail.com>
2025-01-06 23:57:37 +0300

    fix

--- src/features/api/apiSlice.jsx
+++ src/features/api/apiSlice.jsx
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  baseQuery: fetchBaseQuery({ baseUrl: '' }),
   endpoints: (builder) => ({
     getTest: builder.query({
-      query: () => '/data',
+      query: () => 'https://kaleidoscopic-fenglisu-1d45cd.netlify.app',
     }),
   }),
 });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,7 +2,7 @@ import { http, HttpResponse } from 'msw'
 import data from './data/data.json'
 
 export const handlers = [
-  http.get('/fakeApi/data', () => {
+  http.get('https://kaleidoscopic-fenglisu-1d45cd.netlify.app', () => {
     return HttpResponse.json(data)
   }),
 ]
