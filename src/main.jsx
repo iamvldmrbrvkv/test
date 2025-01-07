41a053a51c2a5cd6d0bdd918a291f724efad4912
Владимир Боровиков <iamvladimirborovikov@gmail.com>
2025-01-07 13:59:39 +0300

    production fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './app/store'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 async function enableMocking() {
- /*  if (process.env.NODE_ENV !== 'development') {
+  /*  if (process.env.NODE_ENV !== 'development') {
     return
   } */
- 
-  const { worker } = await import('./mocks/browser.js')
- 
+  
+  const { worker } = await import('./mocks/browser.js');
+  
   return worker.start({
     onUnhandledRequest: 'bypass',
-  })
+  });
 }
- 
+
 enableMocking().then(() => {
   createRoot(document.getElementById('root')).render(
     <StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
-    </StrictMode>)
-})
+    </StrictMode>
+  );
+});
